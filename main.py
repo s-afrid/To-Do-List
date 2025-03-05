800aeb3ff209bbedf5498d4ad908b9f1fd798a20
syed-afrid <afrid0052@gmail.com>
2025-03-05 22:37:30 +0530

    Add button created

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
-
+#Tasks
+def add_to_list():
+    pass
 window = Tk()
 #Window Settings
 window.geometry("800x600")
@@ -8,5 +10,14 @@ app_icon = PhotoImage(file="images/app_icon.png")
 window.iconphoto(True, app_icon)
 window.config(bg="#b1c7c7")
 #Window Widgets
-
+#Add button
+add_button = Button(window,
+                    text="Add",
+                    command=add_to_list,
+                    relief=FLAT,
+                    fg="white",
+                    bg="black",
+                    activeforeground="white",
+                    activebackground="black")
+add_button.place(x=700,y=50)
 window.mainloop()
